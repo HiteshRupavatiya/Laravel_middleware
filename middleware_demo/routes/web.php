25972d7c8fc414de4b04e4273dc576bc34574c1d
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 11:12:46 +0530

    added group routes to access group middleware and route middleware for route only access

--- middleware_demo/routes/web.php
+++ middleware_demo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Middleware Access in Group Of Routes
+
+Route::group(['middleware : age'], function () { 
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/users', [UserController::class, 'index'])->name('user.index')->middleware('age');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/users', [UserController::class, 'index'])->name('user.index')->middleware('age'); // Middleware Access In Single Route
